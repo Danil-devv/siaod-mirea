8679b32d9c3d192607bafb0d3b02644702161517
Danil <s3menovd4nil@yandex.ru>
2023-10-30 17:41:41 +0400

    add practices 1-4

---
+++ prak_2.cpp
@@ -0,0 +1,179 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+vector<pair<int, int>> v; // ключ -> позиция в файле
+
+struct student {
+    int gradebook;
+    int group;
+    char surname[50];
+};
+
+void createBinFile(ifstream& fin, ofstream& fout) {
+    student s;
+    while (!fin.eof()) {
+        fin >> s.gradebook;
+        fin >> s.group;
+        fin >> s.surname;
+        fout.write((char*)&s, sizeof(student));
+    }
+}
+
+void outBinFile(ifstream& fin, int count) {
+    student s;
+    int c = 0;
+    while (!fin.eof() && c < count) {
+        fin.read((char*)&s, sizeof(student));
+        c++;
+        cout << "{" << s.gradebook << " | " << s.group << " | " << s.surname << "}" << "\n";
+    }
+}
+
+void GenerateTable(ifstream& fin, int count) {
+    student s;
+    int c = 0;
+    while (!fin.eof() && c < count) {
+        fin.read((char*)&s, sizeof(student));
+        c++;
+        v.push_back(make_pair(s.gradebook, (c - 1) * sizeof(student)));
+    }
+    sort(v.begin(), v.end());
+}
+
+void generateTxtFile(ofstream& fout, int count) {
+    set<int> st;
+    while (st.size() < count) {
+        auto r = rand() % 1000000 + 100;
+        st.insert(r);
+    }
+
+    vector<int> v;
+    auto it = st.begin();
+    while (it != st.end()) {
+        v.push_back(*it);
+        it++;
+    }
+    random_shuffle(v.begin(), v.end());
+
+    for (int i = 0; i < v.size(); i++) {
+        int u = v[i];
+        fout << u << "\n";
+        fout << u + rand() << "\n";
+        fout << "test" + to_string(u);
+        if (i + 1 < v.size()) fout << "\n";
+    }
+}
+
+bool linearSearch(ifstream& fin, int count, int key, student *out) {
+    student s;
+    int c = 0;
+    while (!fin.eof() && c < count) {
+        fin.read((char*)&s, sizeof(student));
+        c++;
+        if (s.gradebook == key) {
+            *out = s;
+            return true;
+        }
+    }
+    return false;
+}
+
+int binSearchInTable(int key) {
+    int l = 0, r = v.size();
+    while (r - l > 0) {
+        int m = (l + r) / 2;
+        if (v[m].first == key) {
+            return v[m].second;
+        }
+        if (v[m].first < key) {
+            l = m+1;
+        }
+        else {
+            r = m;
+        }
+    }
+    return -1;
+}
+
+bool binarySearchInFile(int key, student* out) {
+    int pos = binSearchInTable(key);
+    if (pos == -1) return false;
+    FILE* ptrFile = fopen("in.dat", "r");
+    fseek(ptrFile, pos, SEEK_SET);
+    fread(out, sizeof(student), 1, ptrFile);
+    fclose(ptrFile);
+    return true;
+}
+
+int main() {
+    srand(time(0));
+    remove("in.txt");
+    remove("in.dat");
+
+    int n;
+    cout << "Input table size:  ";
+    cin >> n;
+
+    ifstream fin, finBin;
+    ofstream fout, foutBin;
+
+
+    // генерируем .txt файл
+    fout.open("in.txt");
+    generateTxtFile(fout, n);
+    fout.close();
+
+    // заполняем бинарный файл данными из сгенерированного .txt файла
+    fin.open("in.txt");
+    foutBin.open("in.dat", ios::in | ios::binary | ios::app);
+    createBinFile(fin, foutBin);
+    fin.close();
+    foutBin.close();
+
+    // выводим бинарный файл
+    /*
+    finBin.open("in.dat", ios::out | ios::binary | ios::app);
+    outBinFileAndGenerateTable(finBin, n);
+    finBin.close();
+    */
+
+    // генерируем таблицу смещений
+    finBin.open("in.dat", ios::out | ios::binary | ios::app);
+    GenerateTable(finBin, n);
+    finBin.close();
+
+    cout << v[50].first << "\n";
+    // Тест функции линейного поиска
+    cout << "Input student gradebook to found: ";
+    int key;
+    cin >> key;
+    student res;
+    finBin.open("in.dat", ios::out | ios::binary | ios::app);
+    if (linearSearch(finBin, n, key, &res)) {
+        cout << "Student is found:\n" << res.gradebook << " " << res.group << " " << res.surname << "\n";
+    }
+    else {
+        cout << "Student is not found!\n";
+    }
+    finBin.close();
+
+    // Тест функции бинарного поиска
+    cout << "Input student gradebook to found: ";
+    cin >> key;
+    if (binarySearchInFile(key, &res)) {
+        cout << "Student is found:\n" << res.gradebook << " " << res.group << " " << res.surname << "\n";
+    }
+    else {
+        cout << "Student is not found!\n";
+    }
+    finBin.close();
+    return 0;
+}
---
+++ prak_4.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+string swapBorderWords(string s) {
+    int first = -1, last = -1;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ',') {
+            if (first == -1) first = i;
+            last = i;
+        }
+    }
+
+    string res = "";
+    for (int i = last + 1; i < s.size(); i++) {
+        res += s[i];
+    }
+    for (int i = first; i <= last; i++) {
+        res += s[i];
+    }
+    for (int i = 0; i < first; i++) {
+        res += s[i];
+    }
+    return res;
+}
+
+map<string, int> RobinKarp(string s, vector<string> v) {
+    const unsigned long long k = 911382323;
+    int n = s.size();
+
+    // массив степеней множителя k
+    // pows[i] == k ^ i
+    vector<unsigned long long> pows(n + 1);
+    pows[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        pows[i] = pows[i - 1] * k;
+    }
+
+    // массив хэшей префиксов строки s
+    // h[k] -- хэш префикса длины k
+    vector<unsigned long long> hash(n + 1);
+    hash[0] = s[0] - 'a' + 1;
+    for (int i = 1; i < n; i++) {
+        hash[i] = (hash[i - 1] * k + (s[i] - 'a' + 1));
+    }
+
+    map<string, int> cnt;  // мапа из пар {строка, кол-во вхождений}
+    for (auto t : v) {
+        if (t.size() > s.size()) {
+            cnt[t] = 0;
+            continue;
+        }
+
+        unsigned long long h = t[0] - 'a'+1;
+        for (int i = 1; i < t.size(); i++) {
+            // считаем хэш строки t
+            h = (h * k + (t[i] - 'a' + 1));
+        }
+
+        for (int i = t.size()-1; i < s.size(); i++) {
+            int r = i, l = i - t.size() + 1;
+            unsigned long long substringHash = 0;
+            if (l == 0) substringHash = hash[r];
+            else substringHash = (hash[r] - hash[l - 1] * pows[r - l + 1]);
+
+            // проверяем, что хэши строки t и подстроки s[l..r] совпали
+            if (substringHash == h) {
+                cnt[t]++;
+            }
+        }
+
+    }
+
+    return cnt;
+}
+
+int main() {
+    cout << "first,second,third,fourth,fifth\n";
+    cout << swapBorderWords("first,second,third,fourth,fifth") << "\n";
+
+    cout << "-------------------\n";
+    string s = "abacaba";
+    vector<string> v(5);
+    v[0] = "ab";
+    v[1] = "a";
+    v[2] = "ca";
+    v[3] = "aca";
+    v[4] = "tt";
+
+    map<string, int> res = RobinKarp(s, v);
+    cout << s << "\n";
+    for (auto u : v) {
+        cout << u << ": " << res[u] << "\n";
+    }
+    return 0;
+}
